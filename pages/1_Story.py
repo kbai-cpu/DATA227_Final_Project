4dadd8def9c23aa4e25adb2781da71a5807109c3
Katie Bai <kbai@Taos-MBP.ht.home>
2026-03-07 20:57:51 -0600

    edits

--- pages/1_Story.py
+++ pages/1_Story.py
@@ -23,7 +23,7 @@ st.set_page_config(page_title="Story", layout="wide")
 st.markdown("""
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,700;1,400&display=swap');
-    html, body, [class*="css"], h1, h2, h3, h4, h5, h6, p, li, span, div, button, input, label {
+    html, body, [class*="css"], h1, h2, h3, h4, h5, h6, p, li, div, button, input, label {
         font-family: 'Lora', serif !important;
     }
     </style>
